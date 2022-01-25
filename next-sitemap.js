032d9f0286e298ae76915a9897dba8a9fd5b4ba1
datsukan <s.datsukan@gmail.com>
2022-01-25 21:47:55 +0900

    :bug: fix sitemapの設定を修正

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,13 @@
-import { fqdn } from "~/data/meta";
-
 module.exports = {
-  siteUrl: fqdn,
+  siteUrl: "https://datsukan.me",
   generateRobotsTxt: true,
+  transform: async (config, path) => {
+    return {
+      loc: path === "/root" ? "/" : path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
